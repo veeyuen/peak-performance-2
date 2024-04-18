102692e9afbf59c88aafcf48605e4ce8cd0737a0
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2024-04-18 16:10:31 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -211,11 +211,11 @@ fig.update_polars(
 fig.update_xaxes(title_text="Days", row=1, col=1)
 fig.update_yaxes(title_text="Recovery<br>Activity", range=[1, 10], row=1, col=1)
 fig.update_xaxes(title_text="Days", row=1, col=2)
-fig.update_yaxes(title_text="Nutrition<br>Activity", row=1, col=2)
+fig.update_yaxes(title_text="Nutrition<br>Activity", range=[1, 10], row=1, col=2)
 fig.update_xaxes(title_text="Days", row=2, col=1)
-fig.update_yaxes(title_text="Training<br>Activity", row=2, col=1)
+fig.update_yaxes(title_text="Training<br>Activity", range=[1, 10], row=2, col=1)
 fig.update_xaxes(title_text="Days", row=2, col=2)
-fig.update_yaxes(title_text="Overall<br>Performance", row=2, col=2)
+fig.update_yaxes(title_text="Overall<br>Performance", range=[1, 10], row=2, col=2)
 
 
 
