5288d2c122355d1131cf2b10301f2b3dcd7b5c71
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2024-04-19 19:46:44 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -221,7 +221,7 @@ fig.update_yaxes(title_text="Recovery", range=[1, 10], title_standoff = 10, row=
 fig.update_xaxes(title_text="Days", ticks="inside", tick0=0.0, dtick=30, row=1, col=2)
 fig.update_yaxes(title_text="Nutrition", range=[1, 10], title_standoff = 10, row=1, col=2)
 fig.update_xaxes(title_text="Days", ticks="inside", tick0=0.0, dtick=30, row=2, col=1)
-fig.update_yaxes(title_text="Training", range=[1, 10], title_standoff = 10, row=2, col=1)
+fig.update_yaxes(title_text="Training", range=[1, 10], title_standoff = 10, row=1, col=3)
 fig.update_xaxes(title_text="Days", ticks="inside", tick0=0.0, dtick=30, row=2, col=2)
 fig.update_yaxes(title_text="Overall<br>Performance", range=[1, 10], title_standoff = 10, row=2, col=2)
 
