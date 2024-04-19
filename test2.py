30d57dc235a897dd8b448ebb3f3542fbbb8eba25
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2024-04-19 20:56:50 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -121,7 +121,7 @@ fig.update_layout(
 
     font=dict(
         family="Arial",
-        size=9,  # Set the font size here
+        size=8,  # Set the font size here
         color="White"
     ),
 
