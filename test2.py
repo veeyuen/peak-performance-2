a191e9d69e6754c8c426f401b296a096bde12984
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2024-04-20 13:07:56 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -216,8 +216,8 @@ fig.update_polars(
 
 #fig.update_polars(radialaxis=dict(range=[0, 1]))
 
-fig.update_xaxes(title_text="Days", ticks="inside", tick0=0.0, dtick=30, row=1, col=1)
-fig.update_yaxes(title_text="Recovery", range=[1, 10], title_standoff = 10, row=1, col=1)
+fig.update_xaxes(title_text="Days", ticks="inside", showgrid=True, tick0=0.0, dtick=30, row=1, col=1)
+fig.update_yaxes(title_text="Recovery", range=[1, 10], showgrid=True, title_standoff = 10, row=1, col=1)
 fig.update_xaxes(title_text="Days", ticks="inside", tick0=0.0, dtick=30, row=1, col=2)
 fig.update_yaxes(title_text="Nutrition", range=[1, 10], title_standoff = 10, row=1, col=2)
 fig.update_xaxes(title_text="Days", ticks="inside", tick0=0.0, dtick=30, row=1, col=3)
