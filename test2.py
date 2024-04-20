bdff75ff887f9afec14dc42353835c6bfa30397c
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2024-04-20 13:27:03 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -218,7 +218,7 @@ fig.update_polars(
 
 fig.update_xaxes(title_text="Days", ticks="inside", showgrid=True, showline=True, linewidth=1, linecolor='white', mirror=True, tick0=0.0, dtick=30, row=1, col=1)
 fig.update_yaxes(title_text="Recovery", range=[1, 10], showgrid=True, showline=True, linewidth=1, linecolor='white', mirror=True, title_standoff = 10, row=1, col=1)
-fig.update_xaxes(title_text="Days", ticks="inside", tick0=0.0, showline=True, linewidth=1, linecolor='white', mirrow=True, showgrid=True, dtick=30, row=1, col=2)
+fig.update_xaxes(title_text="Days", ticks="inside", tick0=0.0, showline=True, linewidth=1, linecolor='white', mirror=True, showgrid=True, dtick=30, row=1, col=2)
 fig.update_yaxes(title_text="Nutrition", range=[1, 10], showgrid=True, showline=True, linewidth=1, linecolor='white', mirror=True, title_standoff = 10, row=1, col=2)
 fig.update_xaxes(title_text="Days", ticks="inside", showgrid=True, showline=True, linewidth=1, linecolor='white', mirror=True, tick0=0.0, dtick=30, row=1, col=3)
 fig.update_yaxes(title_text="Training", range=[1, 10], showgrid=True, showline=True, linewidth=1, linecolor='white', mirror=True, title_standoff = 10, row=1, col=3)
