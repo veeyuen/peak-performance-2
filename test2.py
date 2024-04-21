71a54d9bf5ff98442d763cde5ca5d7fe8c90131d
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2024-04-21 14:05:44 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -35,7 +35,7 @@ fig = make_subplots(
     specs=[[{"type": "scatter"}, {"type": "scatter"}, {"type": "scatter"}],
            [{"colspan": 3}, None, None]
               ],
-    horizontal_spacing= 0.08, vertical_spacing= 0.18,
+    horizontal_spacing= 0.08, vertical_spacing= 0.20,
     row_heights=[0.25, 0.25],
     subplot_titles=("Recovery", "Nutrition", "Exercise")
     )
