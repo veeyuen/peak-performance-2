4aaee7eea3745d61a8295f7996aa80c5abd9fc3e
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2024-04-21 13:36:24 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -35,7 +35,7 @@ fig = make_subplots(
     specs=[[{"type": "scatter"}, {"type": "scatter"}, {"type": "scatter"}],
            [{"colspan": 3}, None, None]
               ],
-    horizontal_spacing= 0.10, vertical_spacing= 0.20,
+    horizontal_spacing= 0.05, vertical_spacing= 0.20,
     row_heights=[0.25, 0.25]
     )
 
