075615eabca64eabba3952167afdab4a6b00f493
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2024-04-28 12:18:57 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -123,7 +123,7 @@ fig.update_layout(
 
     margin_autoexpand=False,
 
-    margin=dict(l=35, r=110, t=30, b=25, pad=3),
+    margin=dict(l=40, r=110, t=30, b=25, pad=3),
 
     grid_domain_x=(0,1.0),
 
