c19dc8afb744fbec1b51239110ad3749c820e22d
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2024-04-28 12:17:39 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -109,7 +109,7 @@ fig.update_layout(
     autosize=False,
 #    minreducedwidth=350,
 #    minreducedheight=350,
-    width=950,
+    width=850,
     height=530,
 #    title_text = 'PEAK PERFORMANCE',
 
